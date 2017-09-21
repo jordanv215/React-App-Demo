6a29001256e7329f5446da71edb79029fc502867
jordanv215 <jordanv215@gmail.com>
2017-09-21 17:41:47 -0600

    ADDED basic Timer component content

---
+++ basic-toggl-clone/toggl/src/Timer.js
@@ -0,0 +1,16 @@
+import React { Component } from 'react';
+// import React and Component functionality from react
+
+// create the react component
+class Timer extends Component {
+  render(){
+    return(
+      <div>
+
+      </div>
+    );
+  }
+}
+
+// export the timer component
+export Default Timer;
