2e804214481026d4143f6604dadc82d2d071b771
jordanv215 <jordanv215@gmail.com>
2017-09-21 22:56:25 -0600

    ADDED functions for start, stop, and reset

--- basic-toggl-clone/toggl/src/Timer.js
+++ basic-toggl-clone/toggl/src/Timer.js
@@ -3,6 +3,33 @@ import React, { Component } from 'react';
 
 // create the react component
 class Timer extends Component {
+
+// using Component life cycle - this will trigger right after Timer has finished loading onto the page.
+  componentDidMount(){
+    this.start
+  }
+
+  // let's start creating a couple of functions to handle the desired functionality of the application
+  stop(){
+    clearInterval(this.timer)
+  }
+
+  start(){
+  this.stop()
+  this.timer = setInterval(this.ticker, 1000)
+  }
+
+  reset(){
+    this.stop()
+    this.setState({clock: 0})
+  }
+
+  ticker(){
+    const newTime = this.state.clock+1
+    this.setState({clock: newTime})
+  }
+
+
   render(){
     return(
       <div>
