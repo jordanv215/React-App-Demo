ba2896c05a50e7cbcc005ca46df425f513108245
jordanv215 <jordanv215@gmail.com>
2017-09-22 09:38:10 -0600

    ADDED comments

--- basic-toggl-clone/toggl/src/Timer.js
+++ basic-toggl-clone/toggl/src/Timer.js
@@ -7,7 +7,7 @@ class Timer extends Component {
 // using Component life cycle - this will trigger right after Timer has finished loading onto the page.
   componentDidMount(){
     // in this case, start the prop as soon as Timer loads.
-    this.start
+    this.start()
   }
 
   // let's start creating a couple of functions to handle the desired functionality of the application
@@ -17,28 +17,37 @@ class Timer extends Component {
   }
 
   start(){
+    // used so you can't hit stop multiple times and add seconds per click
   this.stop()
+  // 1000 milliseconds
   this.timer = setInterval(this.ticker, 1000)
   }
 
+  // stops timer, then sets clock back to 0
   reset(){
     this.stop()
     this.setState({clock: 0})
   }
 
+  // function that is called every second and then adds 1 second
   ticker(){
     const newTime = this.state.clock+1
     this.setState({clock: newTime})
   }
 
+  // initializes the object
   constructor(props){
+    // gives you access to this prop in the constructor
   super(props);
 
   this.state = {
     clock: 0
   };
 
-  // binding
+  // binding gives you access to the global object that you're in INSIDE the function
+  // You can also do this
+  // stop = ()=>{
+  // <button onClick={()=>this.stop()}>Stop</button>
   this.ticker = this.ticker.bind(this);
   this.stop = this.stop.bind(this);
   this.start = this.start.bind(this);
